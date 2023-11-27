1ef28a96991882f9e8b5dae3c0cbab6f4abf5d9b
Christian Raue <captain_2@gmx.de>
2023-11-27 17:21:25 +0100

    implemented server

---
+++ services/code_analyzer/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, request
+from code_analyzer import CodeAnalyzer
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'])
+def route_index():
+    return {'message': "Hello, world!\n This is 'code_analyzer'"}, 200
+
+
+@app.route("/analyze", methods=['GET'])
+def route_analyze():
+    if (data := request.get_json()) is None:
+        return {'message': "no json body"}, 400
+    if (code := data.get('code')) is None:
+        return {'message': "no code"}, 400
+
+    analyzer = CodeAnalyzer(code)
+    analyzer.analyze()
+    return analyzer.to_json(), 200
