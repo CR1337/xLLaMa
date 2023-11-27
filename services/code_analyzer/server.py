34c6c0fa2278f84d2aa7df5f94668ad87f098614
Christian Raue <captain_2@gmx.de>
2023-11-27 18:05:39 +0100

    added app run

--- services/code_analyzer/server.py
+++ services/code_analyzer/server.py
@@ -20,3 +20,7 @@ def route_analyze():
     analyzer = CodeAnalyzer(code)
     analyzer.analyze()
     return analyzer.to_json(), 200
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
