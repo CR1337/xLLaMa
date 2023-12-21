4439ed7444e67b9ad0722749b30d0447aa452940
lkursawe <lara.kursawe@uni-potsdam.de>
2023-12-21 15:08:07 +0100

    added some styling

--- services/frontend/src/components/MainPage.vue
+++ services/frontend/src/components/MainPage.vue
@@ -1,4 +1,48 @@
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Poppins', sans-serif;
+}
+
+h1 {
+    padding: 5px;
+}
+
+div {
+    margin: 5px;
+    padding: 5px;
+}
+
+button {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+
+label {
+    margin-right: 10px;
+}
+
+.radio-container input {
+    margin-right: 3px;
+}
+
+
+
+
+
 </style>
 
 <template>
