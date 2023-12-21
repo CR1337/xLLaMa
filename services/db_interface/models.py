64bfd8407f243e44b3d0700c5336947c0f898a21
Christian Raue <captain_2@gmx.de>
2023-12-21 09:20:21 +0100

    updated documentation

--- services/db_interface/models.py
+++ services/db_interface/models.py
@@ -6,25 +6,25 @@ from model import Prediction, FollowUp, FrameworkItem, Llm, PromptPart, \
 from typing import Dict, Type
 
 models: Dict[str, Type[BaseModel]] = {
-    "predictions": Prediction,
-    "follow_ups": FollowUp,
-    "follow_up_types": FollowUpType,
-    "framework_items": FrameworkItem,
-    "llms": Llm,
-    "prompt_parts": PromptPart,
-    "system_prompts": SystemPrompt,
-    "stop_sequences": StopSequence,
     "code_snippets": CodeSnippet,
-    "user_ratings": UserRating,
+    "follow_up_types": FollowUpType,
+    "follow_ups": FollowUp,
+    "framework_items": FrameworkItem,
     "frameworks": Framework,
+    "llms": Llm,
+    "predictions": Prediction,
     "prompt_part_usages": PromptPartUsage,
+    "prompt_parts": PromptPart,
     "stop_sequence_usages": StopSequenceUsage,
-    "symbol_definitions": SymbolDefinition,
-    "symbol_definition_types": SymbolDefinitionType,
-    "symbol_references": SymbolReference,
+    "stop_sequences": StopSequence,
     "symbol_definition_references": SymbolDefinitionReference,
+    "symbol_definition_types": SymbolDefinitionType,
+    "symbol_definitions": SymbolDefinition,
+    "symbol_references": SymbolReference,
+    "system_prompts": SystemPrompt,
     "undefined_symbol_references": UndefinedSymbolReference,
-    "user_rating_types": UserRatingType
+    "user_rating_types": UserRatingType,
+    "user_ratings": UserRating
 }
 
 by_name_models: Dict[str, Type[BaseModel]] = {
